4f67ff694d9c9aa6f81f2e3274e23eb89ab8349d
Megan Dotto <megan.dotto@gmail.com>
2018-05-15 18:29:38 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,15 +1,17 @@
 class MusicImporter
+
   attr_accessor :path
 
   def initialize(path)
     @path = path
-  end
+  end 
 
   def files
-    Dir[@path+"/*.mp3"].map {|file| file.split("/").last}
+    Dir.entries(path).select {|file| file.end_with?(".mp3")}
   end
 
   def import
-    files.each {|file| Song.create_from_filename(file)}
-  end
-end
+    files.each {|file| Song.new_from_filename(file)}
+  end  
+
+end  
