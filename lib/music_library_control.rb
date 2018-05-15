30e31f69b7066e40f50bb8f8979a22ec08fccd1d
Megan Dotto <megan.dotto@gmail.com>
2018-05-15 18:28:46 +0000

    automatically backed up by learn

--- lib/music_library_control.rb
+++ lib/music_library_control.rb
@@ -3,7 +3,7 @@ class MusicLibraryController
   def initialize(path = "./db/mp3s")
     importer = MusicImporter.new(path)
     importer.import
-  end 
+  end
 
   def call
     input = ""
@@ -22,28 +22,28 @@ class MusicLibraryController
       input = gets.strip
 
       case input
-      when "list songs"   
+      when "list songs"
         Song.all.each_with_index{|song, index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
-      when "list artists"  
+      when "list artists"
         Artist.all.each {|artist| puts artist.name }
-      when "list genres"  
-        Genre.all.each {|genre| puts genre.name }  
+      when "list genres"
+        Genre.all.each {|genre| puts genre.name }
       when "play song"
         puts "Which number song?"
         input = gets.strip
         now_playing = Song.all[input.to_i - 1]
         puts "Playing #{now_playing.artist.name} - #{now_playing.name} - #{now_playing.genre.name}"
-      when "list artist"  
+      when "list artist"
         puts "Which Artist?"
         input = gets.strip
         artist = Artist.find_by_name(input)
         artist.songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
-      when "list genre"  
+      when "list genre"
         puts "Which Genre?"
         input = gets.strip
         genre = Genre.find_by_name(input)
-        genre.songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}  
+        genre.songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
       end
-    end    
-  end 
-end 
+    end
+  end
+end
